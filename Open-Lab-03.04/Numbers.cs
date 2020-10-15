5e7ea07d799db5fd1a79feeb3947cb0d41cf561a
pavolhodas <pavolhodas4@gmail.com>
2020-10-15 14:27:06 +0200

    Find out if a number is odd or even

--- Open-Lab-03.04/Numbers.cs
+++ Open-Lab-03.04/Numbers.cs
@@ -9,12 +9,10 @@ namespace Open_Lab_03._04
             
             if (number % 2 == 0)
             {
-                Console.WriteLine(number % 2);
                 return "even";
             }
             else
             {
-                Console.WriteLine(number % 2);
                 return "odd";
             }
             
